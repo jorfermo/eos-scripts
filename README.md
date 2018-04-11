736c7f215478f84cf3b0ad899161577b3cf5c1e9
Jorge Ferrando <jorfermo@gmail.com>
2018-04-11 11:03:05 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # eos-scripts
-EOS Scripts
+Collection of EOS Scripts
+
+## check\_eos\_bp.py
+BP Health check plugin. It follows the [Nagios plugin guidelines](https://nagios-plugins.org/doc/guidelines.html) so it can be used with [Nagios](https://www.nagios.org/
+) or any other compatible monitoring software like [Icinga](https://www.icinga.com/
+) or [Consul](https://www.consul.io
+).
+
+### Dependencies
+* Python3
+
+### Usage
+
+```bash
+./check_eos_bp.py --help
+usage: check_eos_bp.py [-h] [-H HOST] [-p HTTP_PORT] [-p2 P2P_PORT]
+
+Check BP status
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -H HOST, --host HOST  IP or hostname to check. default = localhost
+  -p HTTP_PORT, --http_port HTTP_PORT
+                        HTTP port number. default = 8888
+  -p2 P2P_PORT, --p2p_port P2P_PORT
+                        P2P port number. default = 9876
+```
---
+++ check_eos_bp.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+import sys
+import argparse
+import urllib.request
+from urllib.error import URLError, HTTPError
+import socket
+
+SERVICE_STATUS = {
+    'OK': 0,
+    'WARNING': 1,
+    'CRITICAL': 2,
+    'UNKNOWN': 3
+}
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='Check BP status')
+    parser.add_argument('-H', '--host', default='localhost',
+                       help='IP or hostname to check. default = localhost')
+    parser.add_argument('-p', '--http_port', type=int, default=8888,
+                       help='HTTP port number. default = 8888')
+    parser.add_argument('-p2', '--p2p_port', type=int, default=9876,
+                       help='P2P port number. default = 9876')
+    args = parser.parse_args()
+    HOST = args.host
+    HTTP_PORT = args.http_port
+    P2P_PORT = args.p2p_port
+
+    try:
+        response = urllib.request.urlopen('http://{}:{}/v1/chain/get_info'.format(HOST, HTTP_PORT))
+        j_response = response.read()
+    except HTTPError as e:
+        print('HTTP CRITICAL: The server couldn\'t fulfill the request. Error code: {}'.format(e.code))
+        sys.exit(SERVICE_STATUS['CRITICAL'])
+    except URLError as e:
+        print('HTTP CRITICAL: Failed to reach server. Reason: {}'.format(e.reason))
+        sys.exit(SERVICE_STATUS['CRITICAL'])
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((HOST, P2P_PORT))
+    if result != 0:
+        print('P2P CRITICAL: P2P service DOWN')
+        sys.exit(SERVICE_STATUS['CRITICAL'])
+
+    print('BP Services OK')
+    sys.exit(SERVICE_STATUS['OK'])
+
+if __name__ == "__main__":
+    main(sys.argv)

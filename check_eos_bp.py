b6a02173c7f333db6b8e7cd981c91d123bde87ed
Jorge Ferrando <jorfermo@gmail.com>
2018-04-11 13:54:34 +0200

    Add verbose logging

--- check_eos_bp.py
+++ check_eos_bp.py
@@ -15,23 +15,27 @@ SERVICE_STATUS = {
 
 def main(argv):
     parser = argparse.ArgumentParser(description='Check BP status')
+    parser.add_argument('-v', '--verbose', action='store_true', help = 'Print verbose logging to stdout')
     parser.add_argument('-H', '--host', default='localhost',
                        help='IP or hostname to check. default = localhost')
     parser.add_argument('-p', '--http_port', type=int, default=8888,
                        help='HTTP port number. default = 8888')
     parser.add_argument('-p2', '--p2p_port', type=int, default=9876,
                        help='P2P port number. default = 9876')
-    parser.add_argument('-c', '--check_list', help='Comma separated list of checks to perform. Choices: [http,p2p]. If empty, it performs all the checks')
+    parser.add_argument('-c', '--check_list', help='Comma separated list of checks to perform. Choices: [http,p2p]. If not set it performs all the checks sequentially')
     args = parser.parse_args()
     HOST = args.host
     HTTP_PORT = args.http_port
     P2P_PORT = args.p2p_port
     CHECK_LIST = args.check_list.split(',') if args.check_list else None
+    VERBOSE = args.verbose
 
     if not CHECK_LIST or 'http' in CHECK_LIST:
         try:
             response = urllib.request.urlopen('http://{}:{}/v1/chain/get_info'.format(HOST, HTTP_PORT))
             j_response = response.read()
+            if VERBOSE:
+                print('HTTP response is OK')
         except HTTPError as e:
             print('HTTP CRITICAL: The server couldn\'t fulfill the request. Error code: {}'.format(e.code))
             sys.exit(SERVICE_STATUS['CRITICAL'])
@@ -45,6 +49,8 @@ def main(argv):
         if result != 0:
             print('P2P CRITICAL: P2P service DOWN')
             sys.exit(SERVICE_STATUS['CRITICAL'])
+        if VERBOSE:
+                print('P2P response is OK')
 
     print('BP Services OK')
     sys.exit(SERVICE_STATUS['OK'])
